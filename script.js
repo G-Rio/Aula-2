78d9b45a6cba9619570033340925825fe9dc8cad
Geovanne Rio <geovanne1542rio@gmail.com>
2024-01-30 20:11:17 -0300

    Javascript front end apple

---
+++ script.js
@@ -0,0 +1,50 @@
+// Objetos------------------------------------------------------------
+const verdeCipreste = {
+    nome: 'verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+};
+
+const azulInverno = {
+    nome: 'azul-inverno',
+    pasta: 'imagens-azul-inverno'
+};
+
+const meiaNoite = {
+    nome: 'meia-noite',
+    pasta: 'imagens-meia-noite'
+};
+
+const estelar = {
+    nome: 'estelar',
+    pasta: 'imagens-estelar'
+};
+
+const rosaClaro = {
+    nome: 'rosa-claro',
+    pasta: 'imagens-rosa-claro'
+};
+
+//Array------------------------------------------------------------------
+
+const minhaLista = []
+
+
+
+
+
+
+
+
+
+
+
+
+//OBJETO É DEFINIDO ASSIM         
+/*
+    const caixinha = {
+    nome: 'Geovanne Rio',
+    idade: 30,
+}
+*/
+//PARA ACESSAR UM ELEMENTO DO OBJETO SÓ USAR O PONTO
+//meiaNoite.pasta
