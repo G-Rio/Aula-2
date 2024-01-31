29d3cd47862006dd31bfda3ffad352855901fa65
Geovanne Rio <geovanne1542rio@gmail.com>
2024-01-30 21:14:29 -0300

    New commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//IDs
+const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+const tituloProduto = document.getElementById("titulo-produto")
 // Objetos------------------------------------------------------------
 const verdeCipreste = {
     nome: 'verde-cipreste',
@@ -24,15 +27,37 @@ const rosaClaro = {
     pasta: 'imagens-rosa-claro'
 };
 
-//Array------------------------------------------------------------------
+//Arrays------------------------------------------------------------------
 
-const minhaLista = []
-
-
-
+const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
+const opcoesTamanho = ['41 mm', '45 mm'];
 
 
 
+//Imagem de vizualização 
+let imagemSelecionada = 1;
+                                    //charAt seleciona um caracter
+function trocarImagem(){            //querySelector vc escolhe o campo ou elemento que quer selecionar
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    imagemVisualizacao.src ='./imagens/opcoes-cores/imagens-azul-inverno/imagem-' + imagemSelecionada + '.jpeg';
+}
+
+
+//Tamanho de caixa
+let tamanhoSelecionado = 1;
+
+function trocarTamanho(){
+    //atualizar variavel de controle de tamanho da caixa-----------------------------------------------
+        //Selecionar o id com checked
+    const idOpcaoTamanho = document.querySelector('[name="opcao-tamanho"]:checked').id;
+        //atualiza a variavel
+    tamanhoSelecionado = idOpcaoTamanho.charAt(0);
+    //mudar o titulo do produto------------------------------------------------------------------------
+    tituloProduto.innerText = "Pulseira loop esportiva azul-inverno para caixa de " + opcoesTamanho[tamanhoSelecionado];
+    //mudar o tamanho da imagem de acordo com a opção--------------------------------------------------
+
+}
 
 
 
